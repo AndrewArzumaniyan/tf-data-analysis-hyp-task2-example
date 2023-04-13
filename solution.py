411467d197c8eee16fba65f8ca52f5e711e1f3d6
Andrey Arzumaniyan <88486829+AndrewArzumaniyan@users.noreply.github.com>
2023-04-14 01:26:12 +0600

    update solution

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+from scipy.stats import kruskal
 
 chat_id = 938988157 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.03
-    pvalue = mannwhitneyu(x, y, alternative="less").pvalue
+    pvalue = kruskal(x, y).pvalue
     
     return pvalue < alpha 
