65b944fa1d38d2490ec2ddfc5fc6befdfe4084b9
Andrey Arzumaniyan <88486829+AndrewArzumaniyan@users.noreply.github.com>
2023-04-14 01:01:08 +0600

    update solution

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import mannwhitneyu
 
 chat_id = 938988157 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    pvalue = mannwhitneyu(x, y, alternative="less").pvalue
+    
+    return pvalue < alpha 
